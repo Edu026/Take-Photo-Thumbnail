3243f2b3d8bf108a10140851195c93ad4af63e28
«@Edu026» <edu.amoros26@gmail.com>
2023-12-12 18:28:06 +0100

    2 buttons

--- app/src/main/java/com/example/take_photo_thumbnail/MainActivity.java
+++ app/src/main/java/com/example/take_photo_thumbnail/MainActivity.java
@@ -11,10 +11,16 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -52,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
             });
     Button btnSelect;
     Button btnGalery;
+    String currentPhotoPath;
+
 
     public static int RC_PHOTO_PICKER = 0;
     @Override
@@ -83,4 +91,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+    private File createImageFile() throws  IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 }
